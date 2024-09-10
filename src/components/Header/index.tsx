871b789ac0286e16837c10209b7dd695e6704993
Dharmendra Boddeda <dharmendra.developer6@gmail.com>
2024-09-10 12:05:58 +0530

    updated navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { RxCross1 } from "react-icons/rx";
 import { MenuSection } from "../MenuSection";
+import Link from "next/link";
 
 interface HeaderProps {
   className?: string;
@@ -15,14 +16,17 @@ export const Header = ({ className }: HeaderProps) => {
       className={`fixed z-10 top-0 left-0 right-0 bg-[#E5E3DC]  {${className}}`}
     >
       <div className=" h-20 pl-16 pr-16 flex  items-center justify-between   ">
-        <a href="/" className="font-bold text-xl cursor-pointer text-[#303636]">
+        <Link
+          href="/"
+          className="font-bold text-xl cursor-pointer text-[#303636]"
+        >
           Your Site Title
-        </a>
+        </Link>
         <div className="cursor-pointer hidden lg:flex text-lg items-center pl-1 font-normal space-x-4 ">
-          <a href="/shop">Shop</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
-          <a href="/cart">Cart{cartCount > 0 && `(${cartCount})`}</a>
+          <Link href="/shop">Shop</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/cart">Cart{cartCount > 0 && `(${cartCount})`}</Link>
         </div>
         <div className="lg:hidden flex items-center justify-center space-x-4 font-bold ">
           <a href="/cart">{`(${cartCount})`}</a>
--- src/components/MenuSection/index.tsx
+++ src/components/MenuSection/index.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export const MenuSection = () => {
   return (
     <div className="lg:hidden text-[#495A58] bg-[#E5E3DC] text-3xl font-bold flex flex-col justify-center h-screen w-screen items-center space-y-6 p-4 cursor-pointer">
-      <a href="/shop">Shop</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+      <Link href="/shop">Shop</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
     </div>
   );
 };
